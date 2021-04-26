ab07daeadb6204ea5fe2d1858d2d6eebfd885fe2
wangpengyuan <ceb_pengyuan@163.com>
2021-04-26 11:44:17 +0800

    fix bug

--- demo.py
+++ demo.py
@@ -28,37 +28,6 @@ def main():
     time_cost = round(end-start,3)
     print('time_cost = %s seconds\n'%time_cost) 
 
-    #测试imgUrl
-
-    # data = {"appId":"000000","imgUrl":"http://ceshiweb.aibaoxian.com/ai/static2/demo_img/general_ocr/02.jpg"}
-    # print('testing ....')
-    # start = time.time()
-    # res=requests.post(url="https://api.aibao.com/general_ocr/v2/general_ocr/",data=data)
-    # end = time.time()
-    # result = json.loads(res.text)
-    # json_str = json.dumps(result, indent=4, ensure_ascii=False)
-    # print(json_str)  
-    # time_cost = round(end-start,3)
-    # print('time_cost = %s seconds\n'%time_cost) 
-
-    #测试imgBase64
-
-    # fp = open(sys.argv[1],'rb')
-    # imgbuf = fp.read()
-    # fp.close()
-    # print('testing ....')
-    # start = time.time()
-    # data = {
-    #         "appId":"000000",
-    #         "imgBase64":base64.b64encode(imgbuf)
-    #         }
-    # res=requests.post(url="http://0.0.0.0:8101/v2/general_ocr/",data=data)
-    # end = time.time()
-    # result = json.loads(res.text)
-    # json_str = json.dumps(result, indent=4, ensure_ascii=False)
-    # print(json_str)    
-    # time_cost = round(end-start,3)
-    # print('time_cost = %s seconds\n'%time_cost)
 
 if __name__ == "__main__":
     main()
